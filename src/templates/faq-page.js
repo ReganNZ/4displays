486fead6b31f4a788f5b9b1280c5c1b09bb640dd
Regan Simons <regan.simons@gmail.com>
2017-11-13 20:20:46 +1300

    changed config

--- src/templates/faq-page.js
+++ src/templates/faq-page.js
@@ -5,15 +5,17 @@ export default function Template({ data }) {
   const { markdownRemark: post } = data;
   return (
     <section className="section">
+      <Helmet title={`FAQ | ${post.frontmatter.title}`} />
       <div className="container content">
-          heloo   
+        <h1 className="title is-size-2 has-text-info is-bold-light">{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </section>
   );
 }
 
 export const pageQuery = graphql`
-  query ProductsByPath($path: String!) {
+  query FaqByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
